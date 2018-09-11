7f1de343a1fae06e8d750ed2fe431fd1a1152be2
Chong Thao <tsoogthoj@hotmail.com>
2018-09-10 23:33:39 -0700

    html completed

---
+++ assets/javascript/game.js
@@ -0,0 +1,73 @@
+var twoSides = [rebelAlliance, galaticEmpire]
+
+var stormTrooper = {
+    name = 'Storm Trooper',
+    hp = 100,
+    side = 'Galatic Empire'
+    abilities1 = 'E-11 blaster rifle',
+    abilities2 = '',
+    abilities3 = '',
+}
+
+var darthVader = {
+    name = 'Darth Vader',
+    hp = 100,
+    side = 'Galatic Empire'
+    abilities1 = 'Force Choke',
+    abilities2 = 'Light Saber',
+    abilities3 = 'Force Lightning',
+}
+
+var darthSidious = {
+    name = 'Darth Sidious',
+    hp = 100,
+    side = 'Galatic Empire'
+    abilities1 = 'Force Throw',
+    abilities2 = 'Force Choke',
+    abilities3 = 'Force Lightning',
+}
+
+var darthMaul = {
+    name = 'Darth Maul',
+    hp = 100,
+    side = 'Galatic Empire'
+    abilities1 = 'Double Light Saber',
+    abilities2 = 'Force Choke',
+    abilities3 = 'Force Throw',
+}
+
+var masterYoda = {
+    name = 'Master Yoda',
+    hp = 100,
+    side = 'Rebel Alliance'
+    abilities1 = 'Light Saber',
+    abilities2 = 'Heals',
+    abilities3 = 'Force Push',
+}
+
+var lukeSkywalker = {
+    name = 'Luke Skywalker',
+    hp = 100,
+    side = 'Rebel Alliance'
+    abilities1 = 'Light Saber',
+    abilities2 = 'Deflect',
+    abilities3 = 'Force Push',
+}
+
+var chewbacca = {
+    name = 'Chewbacca',
+    hp = 100,
+    side = 'Rebel Alliance'
+    abilities1 = 'Bowcaster',
+    abilities2 = 'Slam Ground',
+    abilities3 = 'Shock Grenade',
+}
+
+var princessLeia = {
+    name = 'Princess Leia',
+    hp = 100,
+    side = 'Rebel Alliance'
+    abilities1 = 'Pistol Fire',
+    abilities2 = 'Sheild',
+    abilities3 = 'Grenade',
+}
