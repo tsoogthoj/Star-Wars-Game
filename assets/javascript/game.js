e5da8665c4d65180a3f236d64d5b99cef5da44d4
Chong Thao <tsoogthoj@hotmail.com>
2018-09-12 18:14:13 -0700

    selection phase completed

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,83 +1,116 @@
-var twoSides = [rebelAlliance, galaticEmpire]
 
-// characters
+// characters attributes
 var stormTrooper = {
-    name = 'Storm Trooper',
-    hp = 100,
-    side = 'Galatic Empire',
-    abilities1 = 'E-11 blaster rifle',
-    abilities2 = '',
-    abilities3 = '',
+    name: 'Storm Trooper',
+    hp: 100,
+    side: 'Galatic Empire',
+    abilities1: 'E-11 blaster rifle',
+    abilities2: '',
+    abilities3: '',
 }
 
 var darthVader = {
-    name = 'Darth Vader',
-    hp = 100,
-    side = 'Galatic Empire',
-    abilities1 = 'Force Choke',
-    abilities2 = 'Light Saber',
-    abilities3 = 'Force Lightning',
+    name: 'Darth Vader',
+    hp: 100,
+    side: 'Galatic Empire',
+    abilities1: 'Force Choke',
+    abilities2: 'Light Saber',
+    abilities3: 'Force Lightning',
 }
 
 var darthSidious = {
-    name = 'Darth Sidious',
-    hp = 100,
-    side = 'Galatic Empire',
-    abilities1 = 'Force Throw',
-    abilities2 = 'Force Choke',
-    abilities3 = 'Force Lightning',
+    name: 'Darth Sidious',
+    hp: 100,
+    side: 'Galatic Empire',
+    abilities1: 'Force Throw',
+    abilities2: 'Force Choke',
+    abilities3: 'Force Lightning',
 }
 
 var darthMaul = {
-    name = 'Darth Maul',
-    hp = 100,
-    side = 'Galatic Empire',
-    abilities1 = 'Double Light Saber',
-    abilities2 = 'Force Choke',
-    abilities3 = 'Force Throw',
+    name: 'Darth Maul',
+    hp: 100,
+    side: 'Galatic Empire',
+    abilities1: 'Double Light Saber',
+    abilities2: 'Force Choke',
+    abilities3: 'Force Throw',
 }
 
 var masterYoda = {
-    name = 'Master Yoda',
-    hp = 100,
-    side = 'Rebel Alliance',
-    abilities1 = 'Light Saber',
-    abilities2 = 'Heals',
-    abilities3 = 'Force Push',
+    name: 'Master Yoda',
+    hp: 100,
+    side: 'Rebel Alliance',
+    abilities1: 'Light Saber',
+    abilities2: 'Heals',
+    abilities3: 'Force Push',
 }
 
 var lukeSkywalker = {
-    name = 'Luke Skywalker',
-    hp = 100,
-    side = 'Rebel Alliance',
-    abilities1 = 'Light Saber',
-    abilities2 = 'Deflect',
-    abilities3 = 'Force Push',
+    name: 'Luke Skywalker',
+    hp: 100,
+    side: 'Rebel Alliance',
+    abilities1: 'Light Saber',
+    abilities2: 'Deflect',
+    abilities3: 'Force Push',
 }
 
 var chewbacca = {
-    name = 'Chewbacca',
-    hp = 100,
-    side = 'Rebel Alliance',
-    abilities1 = 'Bowcaster',
-    abilities2 = 'Slam Ground',
-    abilities3 = 'Shock Grenade',
+    name: 'Chewbacca',
+    hp: 100,
+    side: 'Rebel Alliance',
+    abilities1: 'Bowcaster',
+    abilities2: 'Slam Ground',
+    abilities3: 'Shock Grenade',
 }
 
 var princessLeia = {
-    name = 'Princess Leia',
-    hp = 100,
-    side = 'Rebel Alliance',
-    abilities1 = 'Pistol Fire',
-    abilities2 = 'Sheild',
-    abilities3 = 'Grenade',
+    name: 'Princess Leia',
+    hp: 100,
+    side: 'Rebel Alliance',
+    abilities1: 'Pistol Fire',
+    abilities2: 'Sheild',
+    abilities3: 'Grenade',
 }
 
+
+
 // selection phase
-    // hide battle phase
+    // show selection phase
+    $('.battlePhase').toggle();
+    for (var i = 0; i < 4; i++){
+        $('#rebelAlliance-' + i).click(function(){
+                // fade selection when clicked
+                $(this).addClass('fadeDiv');
+                // replase player1 with selection
+                var selected = $(this).html();
+                $('#leftField').html(selected);
+                console.log(selected);
+                $('#leftField').children('img').attr('id', 'leftPlayer');
+                $('#leftField').children('div').remove();
+                // fade out selection phase
+                $('.selectionPhase').fadeOut(1000);
+                // fade in battle phase
+                $('.battlePhase').fadeIn(2000);
+        })
+        $('#galaticEmpire-' + i).click(function(){
+            // fade selection when clicked
+            $(this).addClass('fadeDiv');
+            // replase player1 with selection
+            var selected = $(this).html();
+            $('#leftField').html(selected);
+            console.log(selected);
+            $('#leftField').children('img').attr('id', 'leftPlayer');
+            $('#leftField').children('div').remove();
+            // fade out selection phase
+            $('.selectionPhase').fadeOut(1000);
+            // fade in battle phase
+            $('.battlePhase').fadeIn(2000);
+        });
+    }
 
-    
 
+
+// Battle Phase
 // header portion
     // player one
         // name
